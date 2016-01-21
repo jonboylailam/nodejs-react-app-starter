92c60f9bbf19f74945fe2a3fcbc60373ef6802e3
Jon Lai Lam <jon@lailam.co.za>
2016-01-21 17:37:16 +0200

    Logstash logger dies silently when it cannot connect to the logstash server. Logger is now more configurable too.

--- server/logging.js
+++ server/logging.js
@@ -3,25 +3,39 @@ var _ = require('underscore');
 var winston = require('winston');
 require('winston-logstash');
 
-var winstonLogger = stampit.init(function (ctx) {
+var logstashHost = process.env.LOGSTASH_HOST || '127.0.0.1';
+var logstashPort = process.env.LOGSTASH_PORT || 28777;
+var logstashLogLevel = process.env.LOGSTASH_LOG_LEVEL || 'silly';
+var consoleLogLevel = process.env.CONSOLE_LOG_LEVEL || 'silly';
+
+var WinstonLogger = stampit.init(function (ctx) {
   // Avoid creating more than one winston logger
   if (!ctx.stamp.fixed.methods.winstonLogger) {
-
+    console.log("Creating winston logger");
     var logstashOptions = {
-      level: 'silly',
-      port: 28777,
+      level: logstashLogLevel,
+      port: logstashPort,
       node_name: 'logit-node-name',
-      host: '127.0.0.1'
+      host: logstashHost
     };
+
+    var winstonLogstash = new winston.transports.Logstash(logstashOptions);
+    winstonLogstash.on('error', function() {
+      console.log("Cannot connect to logstash server, going to continue in silent mode");
+    });
 
     ctx.stamp.fixed.methods.winstonLogger = new (winston.Logger)({
       transports: [
-        new (winston.transports.Console)({level: 'silly'}),
-        new (winston.transports.Logstash)(logstashOptions)
+        new (winston.transports.Console)({level: consoleLogLevel}),
+        winstonLogstash
       ]
     });
+
   }
+  return ctx.stamp.fixed.methods.winstonLogger;
 });
+
+var winstonLogger = WinstonLogger();
 
 var logStamp = stampit.methods({
   mergeMetaData: function (args, f) {
@@ -37,29 +51,28 @@ var logStamp = stampit.methods({
   },
   silly: function () {
     var args = [].slice.apply(arguments);
-    this.mergeMetaData(args, this.winstonLogger.silly);
+    this.mergeMetaData(args, winstonLogger.silly);
   },
   log: function () {
     var args = [].slice.apply(arguments);
-    this.mergeMetaData(args, this.winstonLogger.silly);
+    this.mergeMetaData(args, winstonLogger.silly);
   },
   verbose: function () {
     var args = [].slice.apply(arguments);
-    this.mergeMetaData(args, this.winstonLogger.verbose);
+    this.mergeMetaData(args, winstonLogger.verbose);
   },
   info: function () {
     var args = [].slice.apply(arguments);
-    this.mergeMetaData(args, this.winstonLogger.info);
+    this.mergeMetaData(args, winstonLogger.info);
   },
   warn: function () {
     var args = [].slice.apply(arguments);
-    this.mergeMetaData(args, this.winstonLogger.warn);
+    this.mergeMetaData(args, winstonLogger.warn);
   },
   error: function () {
     var args = [].slice.apply(arguments);
-    this.mergeMetaData(args, this.winstonLogger.error);
+    this.mergeMetaData(args, winstonLogger.error);
   }
-}).props({appName: 'web-starter-app', workerId: 'master'});
+}).refs({appName: 'YourAppNameHere', workerId: 'master'});
 
-logStamp = stampit.compose(winstonLogger, logStamp);
 module.exports = logStamp;
