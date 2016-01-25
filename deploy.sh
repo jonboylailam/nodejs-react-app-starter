4177469ea2f75fa61ad39f4e415b84b192343399
jonboylailam <jon@lailam.co.za>
2016-01-25 11:03:32 +0200

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Bundle the app and then scp it to tom.
-# You must login and bounce the app for the changes to take affect.
-
-gulp bundle; scp -r bundle.tar.gz jon@tom:~/apps/ice/.;
